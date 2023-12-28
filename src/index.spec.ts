c53b43ad4b660ae125a5c8e1d90cd97d13e0275e
Carlos Vinicius <viniciusdev.26@gmail.com>
2023-12-28 06:16:56 -0300

    refactor: remove vitest and use bun like a test runner

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,8 @@
-import { beforeAll, expect, it, vi } from "vitest";
+import { expect, it, spyOn } from "bun:test";
 import { log } from "./index";
 
-beforeAll(() => {
-	vi.mock("console.log", vi.fn());
-});
-
 it("should call console.log correctly", () => {
-	const logSpy = vi.spyOn(console, "log");
+	const logSpy = spyOn(console, "log");
 	log();
 
 	expect(logSpy).toHaveBeenCalledWith("Hello via Bun!!!");
